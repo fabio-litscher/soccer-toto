9b729ad7d8aa4435e10d7e99eb2d47adf870850b
fabio-litscher <fabio.litscher@swissonline.ch>
2016-05-17 23:06:36 +0200

    feature added: see winner & topscorer bets on home

--- client/views/home.js
+++ client/views/home.js
@@ -1,3 +1,38 @@
+Template.pots.helpers({
+  'userWithWinner': function() {
+    var exists = Meteor.users.find({ "profile.winner": { $exists: true } }, {});
+    if(exists) {
+      return Meteor.users.find({ "profile.winner": { $exists: true } }, {});
+    }
+  },
+  'userWithTopScorer': function() {
+    var exists = Meteor.users.find({ "profile.topScorer": { $exists: true } }, {});
+    if(exists) {
+      return Meteor.users.find({ "profile.topScorer": { $exists: true } }, {});
+    }
+  },
+  'winnerTeam': function() {
+    return TeamList.findOne({ _id: this.profile.winner }).name;
+
+  },
+  'winnerPot': function() {
+    var exists = PotList.findOne({ name: "winner" }, {});
+    if(exists) {
+      return PotList.findOne({ name: "winner"}, {}).credits;
+    } else {
+      return 0;
+    }
+  },
+  'topScorerPot': function() {
+    var exists = PotList.findOne({ name: "topScorer" }, {});
+    if(exists) {
+      return PotList.findOne({ name: "topScorer"}, {}).credits;
+    } else {
+      return 0;
+    }
+  }
+});
+
 Template.chat.helpers({
   'messages': function() {
     return MessageList.find({}, { sort: { timestamp: -1 }, limit: 200 });
