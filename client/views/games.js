26536649a36e30a265289dd10841c55d4c424c48
fabio-litscher <fabio.litscher@swissonline.ch>
2016-06-15 19:21:13 +0200

    added winning bets to finished games
    all bets are displayed by the finished games with a focus on the correct
    bets

--- client/views/games.js
+++ client/views/games.js
@@ -124,6 +124,22 @@ function getUsername(userId) {
   return Meteor.users.findOne(userId).profile.shortname;
 }
 
+// closedGames template events
+Template.finishedGames.events({
+  'click .showAllGameBets': function(event) {
+    var game = event.currentTarget.getAttribute("gameId");
+    $('#bets_'+game).removeClass('hidden');
+    $('#hideBets_'+game).removeClass('hidden');
+    $('#showBets_'+game).addClass('hidden');
+  },
+  'click .hideAllGameBets': function(event) {
+    var game = event.currentTarget.getAttribute("gameId");
+    $('#bets_'+game).addClass('hidden');
+    $('#hideBets_'+game).addClass('hidden');
+    $('#showBets_'+game).removeClass('hidden');
+  }
+});
+
 // closedGames template helpers
 Template.finishedGames.helpers({
   'closedGames': function() {
@@ -147,5 +163,43 @@ Template.finishedGames.helpers({
   },
   'winnerTeam': function() {
     return TeamList.findOne({ _id: this.knockoutWinner }, {});
+  },
+  'hasBetsForGame': function() {
+    if(BetList.find({ game: this._id }, {}).count() != 0) return true;
+    else return false;
+  },
+  'gameBets': function() {
+    var betsForGame = BetList.find({ game: this._id }, { sort: {user: -1} });
+    var game = GameList.findOne({ _id: this._id });
+    var result1 = game.result1;
+    var result2 = game.result2;
+    var lastuser;
+    var betString = "";
+    betsForGame.forEach(function (row) {
+      if(lastuser == row.user) {
+        if(parseInt(row.result1) == parseInt(result1) && parseInt(row.result2) == parseInt(result2)) {
+          betString = betString + ", " + "<b style='background-color: yellow; color: green;'>" + row.result1 + ":" + row.result2 + "</b>";
+        } else {
+          betString = betString + ", " + row.result1 + ":" + row.result2;
+        }
+      }
+      else if(userExists(row.user)) {
+        if(parseInt(row.result1) == parseInt(result1) && parseInt(row.result2) == parseInt(result2)) {
+          betString = betString + "<br /><b>" + getUsername(row.user) + "</b>: " + "<b style='background-color: yellow; color: green;'>" + row.result1 + ":" + row.result2 + "</b>";
+        } else {
+          betString = betString + "<br /><b>" + getUsername(row.user) + "</b>: " + row.result1 + ":" + row.result2;
+        }
+      }
+      lastuser = row.user;
+    });
+
+    betString = betString + "<br />";
+
+    return Spacebars.SafeString(betString);
+  },
+  'gamePot': function() {
+    var totalBets = BetList.find({ game: this._id }, {}).count();
+    var totalGamePot = totalBets * 2;
+    return totalGamePot;
   }
 });
