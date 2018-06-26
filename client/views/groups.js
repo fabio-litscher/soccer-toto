391a0d1af4bb8483b18cf85d139bef87790c834c
Fabio Litscher <fabio.litscher@sfs.biz>
2018-06-26 06:59:40 +0200

    small bugfix, not completely correct

--- client/views/groups.js
+++ client/views/groups.js
@@ -19,7 +19,7 @@ Template.groupOverview.helpers({
     return GroupList.find({}, {sort: {name: 1} });
   },
   'teams': function() {
-    return TeamList.find({group: this._id}, {sort: {points: -1} }).map(function(document, index) {
+    return TeamList.find({group: this._id}, {sort: {points: -1, goalsMade : -1, goalsGot : 1} }).map(function(document, index) {
       document.index = index + 1;
       return document;
     });
