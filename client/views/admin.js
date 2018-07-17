185cb87af12ee16b99f10c66747cf83ccd261240
Fabio Litscher <fabio.litscher@sfs.biz>
2018-07-17 06:53:57 +0200

    bugfix

--- client/views/admin.js
+++ client/views/admin.js
@@ -174,8 +174,7 @@ Template.useradministration.helpers({
     var tipp1 = this.result1;
     var tipp2 = this.result2;
 
-    if(!result1) return "";
-    else if(tipp1 == result1 && tipp2 == result2) {
+    if(tipp1 == result1 && tipp2 == result2) {
       var totalBets = BetList.find({ game: this.game }, {}).count();
       var totalGamePot = totalBets * 2;
       var countCorrectBets = BetList.find({ game: this.game, result1: result1, result2: result2 }, {}).count();
